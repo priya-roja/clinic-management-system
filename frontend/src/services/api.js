72500488b1275119cc2df667983b20fa47b7d3aa
Priya-roja <priyarojam@gmail.com>
2026-03-08 12:41:43 +0530

    fixed backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://clinic-backend-d1b4.onrender.com/api",
+  baseURL: "https://clinic-backend-d1b4.onrender.com",
 });
 
 API.interceptors.request.use((config) => {
