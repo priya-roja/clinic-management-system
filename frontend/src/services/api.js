95bf32ae3f11f3d03c8d95a64535d06cc1bf68c9
Priya-roja <priyarojam@gmail.com>
2026-03-08 13:52:28 +0530

    fix api path

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://clinic-backend-d1b4.onrender.com",
+  baseURL: "https://clinic-backend-d1b4.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
